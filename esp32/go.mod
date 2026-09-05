8a0117eb2769126fb58f68015b7742a3f1073b9b
Minh Ngo <bbbbbb267@gmail.com>
2026-09-05 16:54:15 +0200

    Webserver on ESP32

---
+++ esp32/go.mod
@@ -0,0 +1,9 @@
+module minhnn.fr/sensor
+
+go 1.26.5
+
+require (
+	github.com/soypat/lneto v0.3.2
+	tinygo.org/x/drivers v0.36.0
+	tinygo.org/x/espradio v0.3.0
+)
---
+++ esp32/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"time"
+
+	"github.com/soypat/lneto/http/httphi"
+	"tinygo.org/x/drivers/netdev"
+	nl "tinygo.org/x/drivers/netlink"
+	link "tinygo.org/x/espradio/netlink"
+)
+
+var (
+	ssid     string
+	password string
+	port     uint16 = 80
+)
+
+func main() {
+	link := &link.Esplink{}
+	netdev.UseNetdev(link)
+
+	println("Connecting to WiFi...", ssid)
+	failIfErr("connect", link.NetConnect(&nl.ConnectParams{
+		Ssid:       ssid,
+		Passphrase: password,
+	}))
+
+	println("Connected to WiFi.")
+	var http httphi.MuxSlice
+	http.Handle("/", func(exch *httphi.Exchange) {
+		exch.RespondString(httphi.StatusOK, "application/json", `{"message":"hello"}`)
+	})
+	var router httphi.Router
+	cfg := httphi.DefaultRouterConfig(4, 2048, http.MaxPathValues())
+	failIfErr("configuring Router", router.Configure(&http, cfg))
+	defer router.Shutdown() // Despawns goroutines.
+	addr, err := link.Addr()
+	failIfErr("Esplink.Addr()", err)
+	print("Hosting webserver on http://", addr.String(), ":", port, "\n")
+	err = link.ListenAndServe(&router, port) // Blocks as long as Router can serve connections.
+	failIfErr("Esplink.ListenAndServe", err)
+}
+
+func failIfErr(action string, err error) {
+	for err != nil {
+		println("fail " + action + ": " + err.Error())
+		time.Sleep(1 * time.Second)
+	}
+}
